217a60a6687dcb11f9b293b003afff678688e1bb
haresh02 <haresh.jayant@gmail.com>
2024-05-07 12:26:44 +0800

    pkce util

--- app/src/main/java/com/example/spotifind/Utils/PKCE.java
+++ app/src/main/java/com/example/spotifind/Utils/PKCE.java
@@ -1,4 +1,25 @@
 package com.example.spotifind.Utils;
 
+import android.util.Base64;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
 public class PKCE {
+
+    public static String generateCodeVerifier(){
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] codeVerifier = new byte[64];
+        secureRandom.nextBytes(codeVerifier);
+        return Base64.encodeToString(codeVerifier, Base64.URL_SAFE | Base64.NO_WRAP | Base64.NO_PADDING);
+    }
+
+    public static String deriveCodeChallenge(String codeVerifier) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] bytes = codeVerifier.getBytes();
+        digest.update(bytes,0, bytes.length);
+        byte[] hashedBytes = digest.digest();
+        return Base64.encodeToString(hashedBytes, Base64.URL_SAFE | Base64.NO_WRAP | Base64.NO_PADDING);
+    }
+    
 }
